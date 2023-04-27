36cd08ae7b0e5b07a737ec9c8b91704905abe136
Vital E <vitalyyemelyanov6@gmail.com>
2023-04-27 10:42:12 +0300

    add test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -26,6 +26,15 @@ public class CashbackHackServiceTest {
 
         Assert.assertEquals(actual,expected);
     }
+    @Test
+    public void testRemain3() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        Assert.assertEquals(actual,expected);
+    }
 
 
 }
